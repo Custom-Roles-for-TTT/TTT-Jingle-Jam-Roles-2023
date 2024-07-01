eabf81e9cba1d266be8001a983bef7abc1ac863f
Malivil <mlebson@outlook.com>
2024-07-01 10:46:22 -0400

    Added ability to limit the ghostwhisperer's ability so only they can see the ghost's chat (disabled by default)

--- lua/customroles/ghostwhisperer.lua
+++ lua/customroles/ghostwhisperer.lua
@@ -22,6 +22,10 @@ table.insert(ROLE.convars, {
     type = ROLE_CONVAR_TYPE_NUM,
     decimal = 0
 })
+table.insert(ROLE.convars, {
+    cvar = "ttt_ghostwhisperer_limited_chat",
+    type = ROLE_CONVAR_TYPE_BOOLEAN
+})
 
 ROLE.translations = {
     ["english"] = {
@@ -30,6 +34,8 @@ ROLE.translations = {
 }
 
 RegisterRole(ROLE)
+
+local ghostwhisperer_limited_chat = CreateConVar("ttt_ghostwhisperer_limited_chat", "0", FCVAR_REPLICATED, "Whether only the ghost whisperer should be able to see the chat of their dead target", 0, 1)
 
 if SERVER then
     hook.Add("TTTPrepareRound", "GhostWhisperer_TTTPrepareRound", function()
@@ -40,6 +46,11 @@ if SERVER then
 
     hook.Add("PlayerCanSeePlayersChat", "GhostWhisperer_PlayerCanSeePlayersChat", function(text, teamOnly, listener, speaker)
         if not IsPlayer(listener) or not IsPlayer(speaker) then return end
+        -- If the listener is dead then let the base logic handle this
+        if not listener:IsActive() then return end
+        -- If this listener is not the ghost whisperer and the chat ability to limited to only them, then let the base logic handle this
+        if ghostwhisperer_limited_chat:GetBool() and not listener:IsGhostWhisperer() then return end
+
         if speaker:Team() == TEAM_SPEC and speaker.TTTIsGhosting then
             return true
         end
@@ -60,7 +71,11 @@ if CLIENT then
         if role == ROLE_GHOSTWHISPERER then
             local roleColor = ROLE_COLORS[ROLE_INNOCENT]
 
-            local html = "The " .. ROLE_STRINGS[ROLE_GHOSTWHISPERER] .. " is a member of the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>innocent team</span> who can use their ghosting device to allow a dead player to talk in chat."
+            local html = "The " .. ROLE_STRINGS[ROLE_GHOSTWHISPERER] .. " is a member of the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>innocent team</span> who can use their ghosting device to allow a dead player to talk"
+            if ghostwhisperer_limited_chat:GetBool() then
+                html = html .. " to them"
+            end
+            html = html .. " in chat."
 
             return html
         end
